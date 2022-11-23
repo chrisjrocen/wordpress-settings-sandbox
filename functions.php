3ade39ebec3bf483bd12f9dc91b130d662ca942d
chrisocen <105217816+chrisocen@users.noreply.github.com>
2022-11-23 17:35:06 +0300

    removed the initial html

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-if (!defined('WP_DEBUG')) {
+if ( !defined('WP_DEBUG' ) ) {
 	die('Direct access forbidden.');
 }
 
@@ -9,34 +9,6 @@ add_action('wp_enqueue_scripts', function () {
 });
 
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-	<title>The Complete Guide To The Settings API | Sandbox Theme</title>
-</head>
-
-
-<body>
-
-	<div id="header">
-		<h1>Sandbox Header</h1>
-	</div><!-- /#header -->
-
-	<div id="content">
-		<p>This is theme content.</p>
-	</div><!-- /#content -->
-
-	<div id="footer">
-		<p>&copy;
-			<?php echo date('Y'); ?> All Rights Reserved.
-		</p>
-	</div><!-- /#footer -->
-
-</body>
-
-</html>
 
 <?php
 
